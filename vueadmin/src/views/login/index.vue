60e4172012890b260979b0f128ce3bfa9c5ea76f
xhj <xhjzrq@163.com>
2020-07-03 14:44:30 +0800

    fitst commit

---
+++ vueadmin/src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="login"> 这里是登录页面xhj
+    </div>
+</template>
+<script>
+    export default {
+        name:"login"
+    }
+</script>
+<style lang="scss" scoped>
+    #login{
+        height: 100vh;  // vh 根据可视窗口来显示得
+        background-color: #344a5f;
+    }
+</style>
