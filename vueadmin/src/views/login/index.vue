3b1c1d7fbc90daab1279d6399de55a6b34da8e3c
xhj <xhjzrq@163.com>
2020-07-03 15:30:43 +0800

    fitst commit

--- vueadmin/src/views/login/index.vue
+++ vueadmin/src/views/login/index.vue
@@ -1,15 +1,50 @@
 <template>
-    <div id="login"> 这里是登录页面xhj
+    <div id="login">
+        <div class="login-wrap">
+            <ul class="menu-tab">
+                <li v-for="item in menuTab" :key="item.id" >
+                    {{ item.txt }}
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
     export default {
-        name:"login"
+        name:"login",
+        data(){
+            return{
+                menuTab:[
+                    {txt:"登录"},
+                    {txt:"注册"}
+                ]
+            }
+
+        }
     }
 </script>
 <style lang="scss" scoped>
     #login{
         height: 100vh;  // vh 根据可视窗口来显示得
         background-color: #344a5f;
+    }
+    .login-wrap {
+        width: 330px;
+        margin: auto;
+    }
+    .menu-tab {
+        text-align: center;
+        li {
+            display: inline-block;
+            width: 88px;
+            line-height: 36px;
+            font-size: 14px;
+            color: #fff;
+            border-radius: 2px;
+            cursor: pointer;
+        }
+        .current {
+            background-color: rgba(0, 0, 0, .1);
+        }
     }
 </style>
